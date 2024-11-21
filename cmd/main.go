444ccb7d3ddf0dddbf088f7fc371d0f358916120
Henocega <ssafonov.dev@gmail.com>
2024-11-22 00:18:25 +0300

    WEEK-2 Add docker file, migration, endpoints implementation

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -10,25 +11,54 @@ import (
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
 
+	"github.com/Henocega/chat-server/internal/config"
+	"github.com/Henocega/chat-server/internal/config/env"
 	chat "github.com/Henocega/chat-server/pkg/chat_v1"
 	"github.com/brianvoe/gofakeit"
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 const grpcPort = 50051
 
 type server struct {
 	chat.UnimplementedChatV1Server
+	pool *pgxpool.Pool
 }
 
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	flag.Parse()
+	ctx := context.Background()
+
+	err := config.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error to load config: %v", err)
+	}
+
+	grpcConfig, err := env.NewGRPCConfig()
+	if err != nil {
+		log.Fatalf("failed to get grpc config: %v", err)
+	}
+
+	pgConfig, err := env.NewPGConfig()
+	if err != nil {
+		log.Fatalf("failed to get pg config: %v", err)
+	}
+
+	lis, err := net.Listen("tcp", grpcConfig.Address())
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
+	pool, err := pgxpool.Connect(ctx, pgConfig.DSN())
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	defer pool.Close()
+
 	s := grpc.NewServer()
 	reflection.Register(s)
-	chat.RegisterChatV1Server(s, &server{})
+	chat.RegisterChatV1Server(s, &server{pool: pool})
 
 	log.Printf("server listening at %v", lis.Addr())
 
@@ -56,4 +86,4 @@ func (s *server) SendMessage(ctx context.Context, req *chat.SendMessageRequest) 
 	fmt.Printf("Send message request: %v", req)
 	log.Printf("Context: %v", ctx)
 	return nil, nil
-}
+}{pool: pool}
